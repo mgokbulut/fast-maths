b8cd1f1ef5047f7b7dfaabe6225f9d49c143fe5b
mgokbulut <43527818+mgokbulut@users.noreply.github.com>
2019-01-13 01:21:02 +0900

    database inserted

--- includes/db_handler-inc.php
+++ includes/db_handler-inc.php
@@ -1,11 +1,18 @@
 <?php
 
-$db_Servername = "localhost";
-$db_Username = "root";
-$db_Password = "";
-$db_Name = "fast_maths";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
 
-$connection = mysqli_connect($db_Servername, $db_Username, $db_Password, $db_Name);
+// $db_Servername = "localhost";
+// $db_Username = "root";
+// $db_Password = "";
+// $db_Name = "fast_maths";
+// $connection = mysqli_connect($db_Servername, $db_Username, $db_Password, $db_Name);
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
