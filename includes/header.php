260f7ab36266e2774e0c58d17221ae8a0a59186c
mgokbulut <43527818+mgokbulut@users.noreply.github.com>
2019-01-14 18:22:40 +0900

    sd'

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,31 @@
 <?php
   include_once("includes/db_handler-inc.php");
-  include_once("includes/update-info.php");
+?>
+
+<?php
+  $id = $_SESSION['user_id'];
+  $username = $_SESSION['username'];
+
+  $query1 = "SELECT * FROM users WHERE username='$username' AND id='$id'";
+  $results = mysqli_query($connection, $query1);
+  if (mysqli_num_rows($results) == 1) {
+    while($row=mysqli_fetch_array($results)){
+      $max_point=$row["max_point"];
+      $id=$row["id"];
+      $lastDate = $row["daily_questions"];
+      $year9Info = $row["year9"];
+      $year10Info = $row["year10"];
+      $year11Info = $row["year11"];
+      $year12Info = $row["year12"];
+    }
+
+    $_SESSION['max_point'] = $max_point;
+    $_SESSION['daily_questions'] = $lastDate;
+    $_SESSION['year9'] = $year9Info;
+    $_SESSION['year10'] = $year10Info;
+    $_SESSION['year11'] = $year11Info;
+    $_SESSION['year12'] = $year12Info;
+  }
 ?>
 
 <?php
