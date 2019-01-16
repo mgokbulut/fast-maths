a54938336fb0a5dab12c04d652d10ef0aa7d4849
mgokbulut <43527818+mgokbulut@users.noreply.github.com>
2019-01-16 23:04:01 +0900

    S

--- game.php
+++ game.php
@@ -71,16 +71,18 @@
         <div class="a1">
           <a href="#" id="quitbutton" onclick="getQuestion()">&laquo; Quit</a>
 
-          <div class="buttons" style="margin-top:10px;">
-  
+          <div id="buttons_div" class="buttons" style="margin-top:10px;">
+
           </div>
 
           <a href="#" class="next" id="nextbutton" onclick="getQuestion()" >Next &raquo;</a>
-
+          <script type="text/javascript">
+            $("#myButton").click(function() {
+              alert("Button code executed.");
+            });
+          </script>
         </div>
     </div>
-  </div>
-
   <div class="rightside">
     <!--Leaderboard-->
     <?php
